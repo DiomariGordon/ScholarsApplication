ab889b4e6d2cac785381887053969bac9e60379c
Kristian <kristiancilento@gmail.com>
2021-08-02 12:05:10 -0400

    tweaked register user page

--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -5,7 +5,7 @@
       <div class="alert alert-danger" role="alert" v-if="registrationErrors">
         {{ registrationErrorMsg }}
       </div>
-      <label for="username" class="sr-only">Username</label>
+      <label for="username" class="sr-only"></label>
       <input
         type="text"
         id="username"
@@ -15,7 +15,7 @@
         required
         autofocus
       />
-      <label for="password" class="sr-only">Password</label>
+      <label for="password" class="sr-only"></label>
       <input
         type="password"
         id="password"
@@ -24,6 +24,7 @@
         v-model="user.password"
         required
       />
+      <label for="confirmPassword" style="display: block;"></label>
       <input
         type="password"
         id="confirmPassword"
@@ -32,7 +33,7 @@
         v-model="user.confirmPassword"
         required
       />
-      <router-link :to="{ name: 'login' }">Have an account?</router-link>
+      <router-link class="rLink" :to="{ name: 'login' }">Have an account?</router-link>
       <button class="btn btn-lg btn-primary btn-block" type="submit">
         Create Account
       </button>
@@ -90,4 +91,8 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.form-register {
+  text-align: center;
+}
+</style>
