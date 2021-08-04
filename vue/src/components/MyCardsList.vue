b3656e72b56562952b6981f1b3022b949bc46b53
Kristian <kristiancilento@gmail.com>
2021-08-04 13:44:23 -0400

    started MyCardsList component

---
+++ vue/src/components/MyCardsList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      <table>
+          <th>My Flashcards</th>
+          <tr v-for="card in myCards" v-bind:key="card.id">
+              <td>{{card.question}}</td>
+          </tr>
+      </table>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'my-cards',
+    data() {
+        return {
+            myCards: this.$store.flashcards
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
