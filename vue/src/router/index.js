25dcf65eda541b559f7861a54c4f24058a050954
Kristian <kristiancilento@gmail.com>
2021-08-10 15:10:15 -0400

    worked on study session

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -14,6 +14,7 @@ import DeckFlashcards from '../views/DeckFlashcards'
 import DeckBoard from '../views/DeckBoard'
 import NewCardForm from '../components/NewCardForm'
 import AddFlashcardToDeck from '../components/AddFlashcardToDeck'
+import SessionCurrentCard from '../views/SessionCurrentCard'
 
 Vue.use(Router)
 
@@ -134,6 +135,11 @@ const router = new Router({
       name: "addFlashcardToDeck", 
       component: AddFlashcardToDeck
 
+    },
+    {
+      path: "/sessionThisCard/:id",
+      name: "sessionThisCard",
+      component: SessionCurrentCard
     }
 
   ]
---
+++ vue/src/views/SessionCurrentCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      <div class="flashcardDisplay">{{this.thisFlashcard.question}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            thisFlashcard: []
+        }
+    },
+    created() {
+        this.thisFlashcard = this.$store.state.flashcards.find((card) =>{
+            return card.flashCardId == this.$route.params.id;
+        });
+    }
+
+}
+</script>
+
+<style>
+
+</style>
